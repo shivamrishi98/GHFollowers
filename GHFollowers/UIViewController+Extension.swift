e2991d0e564b101a13f265278a2facbef9bf1c96
Shivam Rishi <shivamrishisr@gmail.com>
2024-06-22 14:30:51 +1000

    Created GFAlertView

---
+++ GHFollowers/UIViewController+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+Extension.swift
+//  GHFollowers
+//
+//  Created by Shivam Rishi on 22/6/2024.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+    
+}
