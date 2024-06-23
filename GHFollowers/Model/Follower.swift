791c6b9dad578d68c2202b2e0c4fb34c373eb0f7
Shivam Rishi <shivamrishisr@gmail.com>
2024-06-23 16:28:03 +1000

    Added Follower and User Model

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Shivam Rishi on 23/6/2024.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
