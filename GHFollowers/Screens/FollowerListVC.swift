3c73948af9cad37f48779949f55c59cff44e9bc3
Shivam Rishi <shivamrishisr@gmail.com>
2024-06-23 19:31:55 +1000

    Created GetFollowers api

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -15,6 +15,14 @@ class FollowerListVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: errorMessage!, buttonTitle: "Ok")
+                return
+            }
+            
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
