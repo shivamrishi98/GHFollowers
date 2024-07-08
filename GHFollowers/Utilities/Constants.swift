c8499c4b8f303b6cc86ab851df2ff5394454aeb8
Shivam Rishi <shivamrishisr@gmail.com>
2024-07-08 14:30:45 +1000

    Created Child ViewController - GFUserInfoHeaderVC

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Shivam Rishi on 8/7/2024.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
