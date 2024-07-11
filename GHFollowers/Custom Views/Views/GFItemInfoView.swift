107bbd76f537ebc50792ce0f26a297c939f7b161
Shivam Rishi <shivamrishisr@gmail.com>
2024-07-11 13:54:51 +1000

    Created GFItemInfoView

---
+++ GHFollowers/Custom Views/Views/GFItemInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  GFItemInfoView.swift
+//  GHFollowers
+//
+//  Created by Shivam Rishi on 11/7/2024.
+//
+
+import UIKit
+
+class GFItemInfoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
