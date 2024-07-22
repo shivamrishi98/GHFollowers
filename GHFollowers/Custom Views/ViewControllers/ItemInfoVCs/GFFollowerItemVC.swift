4768b9730e9e4faf92c497e21ab3d5303c723e3e
Shivam Rishi <shivamrishisr@gmail.com>
2024-07-22 16:52:40 +1000

    Created GFRepoItemVC and GFFollowerItemVC

---
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,23 @@
+//
+//  GFFollowerItemVC.swift
+//  GHFollowers
+//
+//  Created by Shivam Rishi on 22/7/2024.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, with: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, with: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+    
+}
---
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFRepoItemVC.swift
@@ -0,0 +1,23 @@
+//
+//  GFRepoItemVC.swift
+//  GHFollowers
+//
+//  Created by Shivam Rishi on 22/7/2024.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, with: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, with: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
+}
+
