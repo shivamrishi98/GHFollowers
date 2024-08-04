1a5942fce301ba09a9baf11edf517943d180c799
Shivam Rishi <shivamrishisr@gmail.com>
2024-08-04 16:54:38 +1000

    Optimization: TabBarController, Screen Sizes, Constants

---
+++ GHFollowers/Custom Views/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,34 @@
+//
+//  GFTabBarController.swift
+//  GHFollowers
+//
+//  Created by Shivam Rishi on 4/8/2024.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor = .systemGreen
+        UITabBar.appearance().backgroundColor = .secondarySystemBackground
+        viewControllers = [createSearchNC(), createFavoritesNC()]
+    }
+    
+    func createSearchNC() -> UINavigationController {
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoritesNC() -> UINavigationController {
+        let favoritesListVC = FavoritesListVC()
+        favoritesListVC.title = "Favorites"
+        favoritesListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesListVC)
+    }
+}
